9a5434dc776060cb0e8ac31f5d0c8088a9e4b6a9
Autophagy <mika@crate.io>
2018-01-20 23:04:19 +0100

    Converted response creation into function annotation
    This streamlines the routing code even more, and removes a lot of repetitive code

--- scieldas/app.py
+++ scieldas/app.py
@@ -1,19 +1,27 @@
 from flask import Flask, make_response, redirect, abort
+from functools import wraps
 application = Flask(__name__)
 
 from . import api
 from . import image_creator
 
 
-def create_image_response(image, filetype):
-    if filetype == 'svg':
-        response = make_response(image.tostring())
-        response.content_type = 'image/svg+xml'
-    else:
-        response = make_response(image)
-        response.content_type = 'image/png'
-    response.cache_control.max_age = 60
-    return response
+def create_image_response(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            image, filetype = func(*args, **kwargs)
+            if filetype == 'svg':
+                response = make_response(image.tostring())
+                response.content_type = 'image/svg+xml'
+            else:
+                response = make_response(image)
+                response.content_type = 'image/png'
+            response.cache_control.max_age = 60
+            return response
+        except ValueError:
+            abort(404)
+    return wrapper
 
 @application.route("/")
 def index():
@@ -21,57 +29,39 @@ def index():
 
 # Read The Docs
 @application.route("/rtd/<project>.<filetype>")
+@create_image_response
 def rtd(project, filetype):
-    try:
-        img = image_creator.create_image(filetype, api.get_rtd_build_status, project)
-        return create_image_response(img, filetype)
-    except ValueError:
-        abort(404)
+    return image_creator.create_image(filetype, api.get_rtd_build_status, project), filetype
 
 # Travis
 @application.route("/travis/<user>/<project>.<filetype>")
+@create_image_response
 def travis(user, project, filetype):
-    try:
-        img = image_creator.create_image(filetype, api.get_travis_build_status, user, project)
-        return create_image_response(img, filetype)
-    except ValueError:
-        abort(404)
+    return image_creator.create_image(filetype, api.get_travis_build_status, user, project), filetype
 
 # PyPi
 @application.route("/pypi/version/<project>.<filetype>")
+@create_image_response
 def pypi_version(project, filetype):
-    try:
-        img = image_creator.create_image(filetype, api.get_pypi_version, project)
-        return create_image_response(img, filetype)
-    except ValueError:
-        abort(404)
+    return image_creator.create_image(filetype, api.get_pypi_version, project), filetype
 
 @application.route("/pypi/pyversions/<project>.<filetype>")
+@create_image_response
 def pyversions(project, filetype):
-    try:
-        img = image_creator.create_image(filetype, api.get_pypi_pyversions, project)
-        return create_image_response(img, filetype)
-    except ValueError:
-        abort(404)
+    return image_creator.create_image(filetype, api.get_pypi_pyversions, project), filetype
 
 # Dockerhub
 
 @application.route("/dockerhub/build/<user>/<project>.<filetype>")
+@create_image_response
 def docker_build(user, project, filetype):
-    try:
-        img = image_creator.create_image(filetype, api.get_docker_build_status, user, project)
-        return create_image_response(img, filetype)
-    except ValueError:
-        abort(404)
+    return image_creator.create_image(filetype, api.get_docker_build_status, user, project), filetype
 
 # Licenses
 @application.route("/licenses/<license>.<filetype>")
+@create_image_response
 def licenses(license, filetype):
-    try:
-        img = image_creator.create_image(filetype, api.get_license, license)
-        return create_image_response(img, filetype)
-    except ValueError:
-        abort(404)
+    return image_creator.create_image(filetype, api.get_license, license), filetype
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
